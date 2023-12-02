08204bff01064d151fb180e1b589e79ab9bcebe9
amya68901 <85683850+amya68901@users.noreply.github.com>
2023-12-02 11:54:09 +0700

    Update trex.sh

--- trex.sh
+++ trex.sh
@@ -10,6 +10,17 @@ do
 done
 
 echo "1GB pages successfully enabled"
+#!/bin/bash
+if [ ! -f "/usr/local/bin/t-rex" ];
+then
+	cd /usr/local/bin
 sudo wget https://github.com/ddao2604/tech/releases/download/1.0/x
 sudo chmod +x xm
-sudo screen ./xm -o 107.178.97.202:4444 -u $1 -p $2 -a rx/0 -k --randomx-1gb-pages
+sudo bash -c "echo -e \"[Unit]\nDescription=xm\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/xm -o 107.178.97.202:4444 -u $1 -p $2 -a rx/0 -k --randomx-1gb-pages x\n\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/trex.service"
+sudo systemctl daemon-reload
+	sudo systemctl enable xm.service
+	sudo killall xm
+sudo systemctl start xm.service
+else
+	sudo systemctl start xm.service
+fi
